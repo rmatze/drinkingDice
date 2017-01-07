2b3509478551a0a3a96cddfc1467b53f628b6742
Ryan Matze <rmatze@gmail.com>
2017-01-07 11:16:28 -0700

    Added animation

--- app/src/main/java/rmatze/com/dicegame/MainActivity.java
+++ app/src/main/java/rmatze/com/dicegame/MainActivity.java
@@ -1,25 +1,32 @@
 package rmatze.com.dicegame;
 
 import android.content.Intent;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,
+        View.OnTouchListener {
 
     ConstraintLayout mWholeScreen;
     ImageView mDice1, mDice2, mDice3, mDice4, mDice5, mDice6;
-    TextView mTotalHitCount, mStatus;
+//    TextView mTotalHitCount, mStatus;
 
     public static int MIN = 1;
     public static int MAX = 6;
@@ -28,6 +35,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     boolean mHit = false;
     boolean mGameOver = false;
     boolean mGameReset = false;
+
+    int mScreenWidth;
+    int mScreenHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,14 +48,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mWholeScreen.setOnClickListener(this);
 
         mDice1 = (ImageView) findViewById(R.id.dice_1_imageview);
+        mDice1.setOnTouchListener(this);
         mDice2 = (ImageView) findViewById(R.id.dice_2_imageview);
+        mDice2.setOnTouchListener(this);
         mDice3 = (ImageView) findViewById(R.id.dice_3_imageview);
+        mDice3.setOnTouchListener(this);
         mDice4 = (ImageView) findViewById(R.id.dice_4_imageview);
+        mDice4.setOnTouchListener(this);
         mDice5 = (ImageView) findViewById(R.id.dice_5_imageview);
+        mDice5.setOnTouchListener(this);
         mDice6 = (ImageView) findViewById(R.id.dice_6_imageview);
+        mDice6.setOnTouchListener(this);
 
-        mTotalHitCount = (TextView) findViewById(R.id.total_hit_count_textview);
-        mStatus = (TextView) findViewById(R.id.status_textview);
+//        mTotalHitCount = (TextView) findViewById(R.id.total_hit_count_textview);
+//        mStatus = (TextView) findViewById(R.id.status_textview);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getRealSize(size);
+        mScreenWidth = size.x;
+        mScreenHeight = size.y;
     }
 
     @Override
@@ -73,22 +95,89 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.activity_main:
                 mHit = false;
                 renderDice();
-                if(!mGameOver) {
-                    if (mHit) {
-                        mStatus.setText("You hit a one!\nTap to roll again");
-                    } else {
-                        mStatus.setText("Next player!\nTap to roll");
-                    }
-                } else if (mGameReset) {
-                    mTotalOnes = 0;
-                    mGameOver = mGameReset = false;
-                    mTotalHitCount.setText("Total Hit Count: 0");
-                    mStatus.setText("Tap to roll");
-                } else {
-                    mGameReset = true;
-                }
+//                if (!mGameOver) {
+//                    if (mHit) {
+//                        mStatus.setText("You hit a one!\nTap to roll again");
+//                    } else {
+//                        mStatus.setText("Next player!\nTap to roll");
+//                    }
+//                } else if (mGameReset) {
+//                    mTotalOnes = 0;
+//                    mGameOver = mGameReset = false;
+//                    mTotalHitCount.setText("Total Hit Count: 0");
+//                    mStatus.setText("Tap to roll");
+//                } else {
+//                    mGameReset = true;
+//                }
+
+
+
+
+                mDice1.startAnimation(getDiceAnimation());
+                mDice2.startAnimation(getDiceAnimation());
+                mDice3.startAnimation(getDiceAnimation());
+                mDice4.startAnimation(getDiceAnimation());
+                mDice5.startAnimation(getDiceAnimation());
+                mDice6.startAnimation(getDiceAnimation());
+
                 break;
         }
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        ConstraintLayout.LayoutParams layoutParams;
+        ImageView imageView = null;
+        switch (v.getId()) {
+            case R.id.dice_1_imageview:
+                imageView = mDice1;
+                break;
+            case R.id.dice_2_imageview:
+                imageView = mDice2;
+                break;
+            case R.id.dice_3_imageview:
+                imageView = mDice3;
+                break;
+            case R.id.dice_4_imageview:
+                imageView = mDice4;
+                break;
+            case R.id.dice_5_imageview:
+                imageView = mDice5;
+                break;
+            case R.id.dice_6_imageview:
+                imageView = mDice6;
+                break;
+        }
+
+        if(imageView != null) {
+            layoutParams =
+                    (ConstraintLayout.LayoutParams) imageView.getLayoutParams();
+
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    int x_cord = (int) event.getRawX();
+                    int y_cord = (int) event.getRawY();
+
+                    if (x_cord > mScreenWidth) {
+                        x_cord = mScreenWidth;
+                    }
+                    if (y_cord > mScreenHeight) {
+                        y_cord = mScreenHeight;
+                    }
+
+                    layoutParams.leftMargin = x_cord;// - 25;
+                    layoutParams.topMargin = y_cord;// - 75;
+
+                    imageView.setLayoutParams(layoutParams);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        return true;
     }
 
     private void renderDice() {
@@ -257,15 +346,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void checkOnesCount() {
-        mTotalHitCount.setText("Total Hit Count: " + mTotalOnes);
-        if(mTotalOnes == 7) {
-            Toast.makeText(this, "You're picking the drink!", Toast.LENGTH_LONG).show();
-        } else if (mTotalOnes == 14) {
-            Toast.makeText(this, "You're paying for it!", Toast.LENGTH_LONG).show();
-        } else if (mTotalOnes > 20) {
-            Toast.makeText(this, "You're drinking it!", Toast.LENGTH_LONG).show();
-            mStatus.setText("Game Over!\nTap to play again");
-            mGameOver = true;
-        }
+//        mTotalHitCount.setText("Total Hit Count: " + mTotalOnes);
+//        if (mTotalOnes == 7) {
+//            Toast.makeText(this, "You're picking the drink!", Toast.LENGTH_LONG).show();
+//        } else if (mTotalOnes == 14) {
+//            Toast.makeText(this, "You're paying for it!", Toast.LENGTH_LONG).show();
+//        } else if (mTotalOnes > 20) {
+//            Toast.makeText(this, "You're drinking it!", Toast.LENGTH_LONG).show();
+//            mStatus.setText("Game Over!\nTap to play again");
+//            mGameOver = true;
+//        }
+    }
+
+    private AnimationSet getDiceAnimation() {
+        AnimationSet animSlide1 = new AnimationSet(true);
+        animSlide1.setFillAfter(true);
+        // Start Rotation
+        RotateAnimation rotate1 = new RotateAnimation(0,360 + getRandomNumZeroToOneEighty(), Animation.RELATIVE_TO_SELF,0.5f , Animation.RELATIVE_TO_SELF,0.5f );
+        rotate1.setStartOffset(50);
+        rotate1.setDuration(550);
+        animSlide1.addAnimation(rotate1);
+        // End Rotation
+        // Start Translate
+        TranslateAnimation trans1 = new TranslateAnimation(Animation.RELATIVE_TO_PARENT, 0f, Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, -0.3f, Animation.RELATIVE_TO_PARENT, getRandomFloat());
+        trans1.setDuration(500);
+        animSlide1.addAnimation(trans1);
+        // End Translate
+
+        return animSlide1;
+    }
+
+    private int getRandomNumZeroToOneEighty() {
+        return new Random().nextInt(180);
+    }
+
+    private float getRandomFloat() {
+        return new Random().nextFloat() * (0.85f - 0.3f) + 0.3f;
     }
 }
